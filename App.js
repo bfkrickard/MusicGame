caebd9f530cd6112c9fd2756f4e871d2f78d143d
Daniel Yo <dnl992004@yahoo.com.au>
2020-09-20 19:06:40 -0700

    update: - setup home container - setup initial theme - install react-native-paper

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {HomeContainer} from './src/container/homeContainer'
+import { Provider as PaperProvider } from 'react-native-paper'
+import {theme} from './src/theme'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <PaperProvider theme={theme}>
+      <HomeContainer/>
+    </PaperProvider>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
